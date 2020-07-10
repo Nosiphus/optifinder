678333cdef329b111a8f0f00081cbd0bf68dcc52
Sean Crain <38871906+seancrain@users.noreply.github.com>
2020-07-09 22:50:28 -0500

    Source code update

---
+++ 1.12.2/src/main/java/com/nosiphus/optifinder/Main.java
@@ -0,0 +1,27 @@
+package com.nosiphus.optifinder;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import com.nosiphus.optifinder.init.ScanModsFolder;
+
+@Mod(modid = Main.MODID, name = Main.NAME, version = Main.VERSION)
+public class Main {
+	
+    public static final String MODID = "optifinder";
+    public static final String NAME = "OptiFinder";
+    public static final String VERSION = "1.12.2_HD_U_F6_pre1";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    	ScanModsFolder.main(VERSION);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
---
+++ 1.7.10/src/main/java/com/nosiphus/optifinder/util/Reference.java
@@ -0,0 +1,10 @@
+package com.nosiphus.optifinder.util;
+
+public class Reference {
+
+	public static final String MODID = "optifinder";
+	public static final String NAME = "OptiFinder";
+	public static final String VERSION = "1.7.10_HD_U_E7";
+	public static final String ACCEPTED_VERSIONS = "[1.7.10]";
+
+}
