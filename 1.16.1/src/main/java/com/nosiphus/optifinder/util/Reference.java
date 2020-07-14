9ff7f83fa012466ec0ef753ffeb86aa06c4463c7
Sean Crain <38871906+seancrain@users.noreply.github.com>
2020-07-14 04:16:14 -0500

    Update source for pre4

--- 1.16.1/src/main/java/com/nosiphus/optifinder/util/Reference.java
+++ 1.16.1/src/main/java/com/nosiphus/optifinder/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MODID = "optifinder";
 	public static final String NAME = "OptiFinder";
-	public static final String VERSION = "1.16.1_HD_U_G2_pre3";
+	public static final String VERSION = "1.16.1_HD_U_G2_pre4";
 	public static final String ACCEPTED_VERSIONS = "[1.16.1]";
 
 }
