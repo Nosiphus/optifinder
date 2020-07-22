c8595361fd6c8db79eb856bd6c92b1178648d396
Sean Crain <38871906+seancrain@users.noreply.github.com>
2020-07-22 12:13:38 -0500

    Source code update

--- 1.16.1/src/main/java/com/nosiphus/optifinder/util/Reference.java
+++ 1.16.1/src/main/java/com/nosiphus/optifinder/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MODID = "optifinder";
 	public static final String NAME = "OptiFinder";
-	public static final String VERSION = "1.16.1_HD_U_G2_pre5";
+	public static final String VERSION = "1.16.1_HD_U_G2_pre7";
 	public static final String ACCEPTED_VERSIONS = "[1.16.1]";
 
 }
