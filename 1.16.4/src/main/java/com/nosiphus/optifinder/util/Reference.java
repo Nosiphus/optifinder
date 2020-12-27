bf47d108600957b095124cb060aed26042622e67
Sean Crain <38871906+seancrain@users.noreply.github.com>
2020-12-27 07:23:22 -0600

    G6_pre1

--- 1.16.4/src/main/java/com/nosiphus/optifinder/util/Reference.java
+++ 1.16.4/src/main/java/com/nosiphus/optifinder/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MODID = "optifinder";
 	public static final String NAME = "OptiFinder";
-	public static final String VERSION = "1.16.4_HD_U_G5";
+	public static final String VERSION = "1.16.4_HD_U_G6_pre1";
 	public static final String ACCEPTED_VERSIONS = "[1.16.4]";
 
 }
